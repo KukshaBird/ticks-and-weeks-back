d79f89b11ebf998b04831f173457970980783549
KukshaBird <samoilovartem1989@gmail.com>
2025-03-20 12:15:10 +0200

    Generate base dish module structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DishModule } from './dish/dish.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DishModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
